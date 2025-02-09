662029266b78bdfa30a0349139255e2d94955804
JulienH <julienhalde9@gmail.com>
2025-02-09 16:28:58 -0500

    UI Finished

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -11,6 +11,10 @@ function Swap() {
   const [slippage, setSlippage] = useState(2.5);
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
 
   function handleSlippageChange(e) {
     setSlippage(e.target.value);
@@ -23,6 +27,36 @@ function Swap() {
     // }else{
     //   setTokenTwoAmount(null);
     // }
+  }
+
+  function switchTokens() {
+    // setPrices(null);
+    // setTokenOneAmount(null);
+    // setTokenTwoAmount(null);
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+    // fetchPrices(two.address, one.address);
+  }
+
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
+  function modifyToken(i){
+    // setPrices(null);
+    // setTokenOneAmount(null);
+    // setTokenTwoAmount(null);
+    if (changeToken === 1) {
+      setTokenOne(tokenList[i]);
+      // fetchPrices(tokenList[i].address, tokenTwo.address)
+    } else {
+      setTokenTwo(tokenList[i]);
+      // fetchPrices(tokenOne.address, tokenList[i].address)
+    }
+    setIsOpen(false);
   }
 
   const settings = (
@@ -40,36 +74,69 @@ function Swap() {
 
 
   return (
-    <div className='tradeBox'>
-      <div className='tradeBoxHeader'>
-        <h4>Swap</h4>
-        <Popover
+    <>
+      {/* {contextHolder} */}
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title="Select a token"
+      >
+        <div className="modalContent">
+          {tokenList?.map((e, i) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={i}
+                onClick={() => modifyToken(i)}
+              >
+                <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                <div className="tokenChoiceNames">
+                  <div className="tokenName">{e.name}</div>
+                  <div className="tokenTicker">{e.ticker}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
             content={settings}
             title="Settings"
             trigger="click"
             placement="bottomRight"
-        >
-          <SettingOutlined className='cog' />
-        </Popover>
+          >
+            <SettingOutlined className="cog" />
+          </Popover>
+        </div>
+        <div className="inputs">
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeAmount}
+            // disabled={!prices}
+          />
+          <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
+          </div>
+          <div className="assetOne" onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+          <div className="assetTwo" onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
+        <div className="swapButton" disabled={!tokenOneAmount} >Swap</div>
       </div>
-        <Input
-          placeholder="0"
-          value={tokenOneAmount}
-          onChange={changeAmount}
-          // disabled={!prices}
-        />
-      <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
-      <div className="assetOne">
-        <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
-        {tokenOne.ticker}
-        <DownOutlined />
-      </div>
-      <div className="assetTwo">
-        <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
-        {tokenTwo.ticker}
-        <DownOutlined />
-      </div>
-    </div>
+    </>
   );
 }
 
